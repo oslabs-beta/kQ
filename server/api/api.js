63f2bf9073c662b5487f978b2d4a729871f3badb
Kaiwei Hsu <hkw1988@hotmail.com>
2020-10-19 16:52:18 -0700

    fix producer in kafkaController, and express

--- server/api/api.js
+++ server/api/api.js
@@ -2,26 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 
-const {
-  startKafka,
-  // produce,
-  // consume,
-} = require('../controllers/kafkaController');
+const { produceMessage } = require('../controllers/kafkaController');
 
-const {
-  // startKafka,
-  produce,
-  // consume,
-} = require('../controllers/kafkaProducer');
-
-const {
-  // startKafka,
-  // produce,
-  consume,
-} = require('../controllers/kafkaConsumer');
-
-router.get('/', startKafka, produce, consume, (req, res) => {
+router.post('/message', produceMessage, (req, res) => {
   res.sendStatus(200);
 });
+
+// const {
+//   startKafka,
+//   // produce,
+//   // consume,
+// } = require('../controllers/kafkaController');
+
+// const {
+//   // startKafka,
+//   produce,
+//   // consume,
+// } = require('../controllers/kafkaProducer');
+
+// const {
+//   // startKafka,
+//   // produce,
+//   consume,
+// } = require('../controllers/kafkaConsumer');
+
+// router.get('/', startKafka, produce, consume, (req, res) => {
+//   res.sendStatus(200);
+// });
 
 module.exports = router;
--- server/controllers/kafkaController.js
+++ server/controllers/kafkaController.js
@@ -1,130 +1,139 @@
-// const axios = require('axios');
-// const { Kafka } = require('kafkajs');
-
-// // Create an instance of Kafka
-// // const kafka = new Kafka({
-// //   clientId: 'my-app',
-// //   brokers: ['kafka1:9092', 'kafka2:9092'],
-// // });
-// const kafka = new Kafka({
-//   clientId: 'my-app',
-//   // brokers: async () => {
-//   //   const clusterResponse = await axios
-//   //     .get('https://kafka-rest:8082/v3/clusters', {
-//   //       headers: { 'Content-Type': 'application/vnd.api+json' },
-//   //     })
-//   //     .then((response) => response.json());
-//   //   const clusterUrl = clusterResponse.data[0].links.self;
-
-//   //   const brokersResponse = await axios
-//   //     .get(`${clusterUrl}/brokers`, {
-//   //       headers: { 'Content-Type': 'application/vnd.api+json' },
-//   //     })
-//   //     .then((response) => response.json());
-
-//   //   const brokers = brokersResponse.data.map((broker) => {
-//   //     const { host, port } = broker.attributes;
-//   //     return `${host}:${port}`;
-//   //   });
-
-//   //   return brokers;
-//   // },
-//   brokers: ['jonathans-imac:9092'],
-// });
-
-// const kafkaController = {};
-
-// kafkaController.startKafka = async (req, res, next) => {
-//   console.log('in sk ctrlr');
-
-//   // Produce a message to test-topic
-//   const producer = kafka.producer();
-
-//   await producer.connect();
-//   await producer.send({
-//     topic: 'test-topic',
-//     messages: [{ value: 'Hello KafkaJS user!' }],
-//   });
-
-//   await producer.disconnect();
-
-//   // Create a consumer to subscribe to test-topic
-//   const consumer = kafka.consumer({ groupId: 'test-group' });
-
-//   await consumer.connect();
-//   await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
-
-//   await consumer.run({
-//     eachMessage: async ({ topic, parition, message }) => {
-//       console.log({
-//         value: message.value.toString(),
-//       });
-//     },
-//   });
-
-//   console.log('message done');
-
-//   next();
-// };
-
-// module.exports = kafkaController;
-
 const { Kafka } = require('kafkajs');
 
 const kafkaController = {};
 
-kafkaController.startKafka = async (req, res, next) => {
+kafkaController.produceMessage = async (req, res, next) => {
+  console.log('hi');
   try {
     const kafka = new Kafka({
       clientId: 'myapp',
       brokers: ['Weis-NB.local:9092'],
     });
+    const producer = kafka.producer();
+    console.log('connecting...');
+    await producer.connect();
+    console.log('connected!');
 
-    const admin = kafka.admin();
-    console.log('connecting');
-    await admin.connect();
-    console.log('connected');
-    await admin.createTopics({
-      topics: [
+    const { msg } = req.body;
+
+    const partition = msg[0].toLowerCase() < 'n' ? 0 : 1;
+    const result = await producer.send({
+      topic: 'Users',
+      messages: [
         {
-          topic: 'Users',
-          numPartitions: 2,
+          value: msg,
+          partition: partition,
         },
       ],
     });
-    console.log('created successfully');
-    // console.log(kafka);
-    await admin.disconnect();
-  } catch (ex) {
-    console.error(`Something bad happened ${ex}`);
+    console.log(`send successfully! ${JSON.stringify(result)}`);
+    await producer.disconnect();
+  } catch (err) {
+    console.error(`something bad happened ${err}`);
   } finally {
     next();
   }
 };
 
-// kafkaController.produce = async (req, res, next) => {
+module.exports = kafkaController;
+
+// // const axios = require('axios');
+// // const { Kafka } = require('kafkajs');
+
+// // // Create an instance of Kafka
+// // // const kafka = new Kafka({
+// // //   clientId: 'my-app',
+// // //   brokers: ['kafka1:9092', 'kafka2:9092'],
+// // // });
+// // const kafka = new Kafka({
+// //   clientId: 'my-app',
+// //   // brokers: async () => {
+// //   //   const clusterResponse = await axios
+// //   //     .get('https://kafka-rest:8082/v3/clusters', {
+// //   //       headers: { 'Content-Type': 'application/vnd.api+json' },
+// //   //     })
+// //   //     .then((response) => response.json());
+// //   //   const clusterUrl = clusterResponse.data[0].links.self;
+
+// //   //   const brokersResponse = await axios
+// //   //     .get(`${clusterUrl}/brokers`, {
+// //   //       headers: { 'Content-Type': 'application/vnd.api+json' },
+// //   //     })
+// //   //     .then((response) => response.json());
+
+// //   //   const brokers = brokersResponse.data.map((broker) => {
+// //   //     const { host, port } = broker.attributes;
+// //   //     return `${host}:${port}`;
+// //   //   });
+
+// //   //   return brokers;
+// //   // },
+// //   brokers: ['jonathans-imac:9092'],
+// // });
+
+// // const kafkaController = {};
+
+// // kafkaController.startKafka = async (req, res, next) => {
+// //   console.log('in sk ctrlr');
+
+// //   // Produce a message to test-topic
+// //   const producer = kafka.producer();
+
+// //   await producer.connect();
+// //   await producer.send({
+// //     topic: 'test-topic',
+// //     messages: [{ value: 'Hello KafkaJS user!' }],
+// //   });
+
+// //   await producer.disconnect();
+
+// //   // Create a consumer to subscribe to test-topic
+// //   const consumer = kafka.consumer({ groupId: 'test-group' });
+
+// //   await consumer.connect();
+// //   await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+
+// //   await consumer.run({
+// //     eachMessage: async ({ topic, parition, message }) => {
+// //       console.log({
+// //         value: message.value.toString(),
+// //       });
+// //     },
+// //   });
+
+// //   console.log('message done');
+
+// //   next();
+// // };
+
+// // module.exports = kafkaController;
+
+// const { Kafka } = require('kafkajs');
+
+// const kafkaController = {};
+
+// kafkaController.startKafka = async (req, res, next) => {
 //   try {
 //     const kafka = new Kafka({
 //       clientId: 'myapp',
 //       brokers: ['Weis-NB.local:9092'],
 //     });
 
-//     const producer = kafka.producer();
-//     console.log('Connecting');
-//     await producer.connect();
-//     console.log('Connected');
-//     const result = await producer.send({
-//       topic: 'Users',
-//       messages: [
+//     const admin = kafka.admin();
+//     console.log('connecting');
+//     await admin.connect();
+//     console.log('connected');
+//     await admin.createTopics({
+//       topics: [
 //         {
-//           value: 'msg',
-//           partition: 0,
+//           topic: 'Users',
+//           numPartitions: 2,
 //         },
 //       ],
 //     });
-
-//     console.log(`send successfully ${JSON.stringify(result)}`);
-//     await producer.disconnect();
+//     console.log('created successfully');
+//     // console.log(kafka);
+//     await admin.disconnect();
 //   } catch (ex) {
 //     console.error(`Something bad happened ${ex}`);
 //   } finally {
@@ -132,39 +141,69 @@ kafkaController.startKafka = async (req, res, next) => {
 //   }
 // };
 
-// kafkaController.consume = async (req, res, next) => {
-//   try {
-//     const kafka = new Kafka({
-//       clientId: 'myapp',
-//       brokers: ['Weis-NB.local:9092'],
-//     });
+// // kafkaController.produce = async (req, res, next) => {
+// //   try {
+// //     const kafka = new Kafka({
+// //       clientId: 'myapp',
+// //       brokers: ['Weis-NB.local:9092'],
+// //     });
 
-//     const consumer = kafka.consumer({
-//       groupId: 'test',
-//     });
-//     console.log('Connecting');
-//     await consumer.connect();
-//     console.log('Connected');
-//     consumer.subscribe({
-//       topic: 'Users',
-//       fromBeginning: true,
-//     });
-//     await consumer.run({
-//       eachMessage: async (result) => {
-//         console.log(
-//           `received msg from ${result.message.value} on partition ${result.partition}`
-//         );
-//       },
-//     });
-//   } catch (ex) {
-//     console.error(`Something bad happened ${ex}`);
-//   } finally {
-//     next();
-//   }
-// };
+// //     const producer = kafka.producer();
+// //     console.log('Connecting');
+// //     await producer.connect();
+// //     console.log('Connected');
+// //     const result = await producer.send({
+// //       topic: 'Users',
+// //       messages: [
+// //         {
+// //           value: 'msg',
+// //           partition: 0,
+// //         },
+// //       ],
+// //     });
 
-run();
+// //     console.log(`send successfully ${JSON.stringify(result)}`);
+// //     await producer.disconnect();
+// //   } catch (ex) {
+// //     console.error(`Something bad happened ${ex}`);
+// //   } finally {
+// //     next();
+// //   }
+// // };
 
-async function run() {}
+// // kafkaController.consume = async (req, res, next) => {
+// //   try {
+// //     const kafka = new Kafka({
+// //       clientId: 'myapp',
+// //       brokers: ['Weis-NB.local:9092'],
+// //     });
 
-module.exports = kafkaController;
+// //     const consumer = kafka.consumer({
+// //       groupId: 'test',
+// //     });
+// //     console.log('Connecting');
+// //     await consumer.connect();
+// //     console.log('Connected');
+// //     consumer.subscribe({
+// //       topic: 'Users',
+// //       fromBeginning: true,
+// //     });
+// //     await consumer.run({
+// //       eachMessage: async (result) => {
+// //         console.log(
+// //           `received msg from ${result.message.value} on partition ${result.partition}`
+// //         );
+// //       },
+// //     });
+// //   } catch (ex) {
+// //     console.error(`Something bad happened ${ex}`);
+// //   } finally {
+// //     next();
+// //   }
+// // };
+
+// run();
+
+// async function run() {}
+
+// module.exports = kafkaController;

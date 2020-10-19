60a3290a56b89ddf913774de35e7e58866a19995
shreshth <shreshthsrivastava2@gmail.com>
2020-10-19 16:23:14 -0400

    finished writing Go script to generate large amounts of data and send it to the Express server

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.jsx';
+// import styles from './scss/styles.scss';
+
+render(<App />, document.getElementById('root'));

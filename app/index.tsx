99de74cf30e138db8a98cd8f5f922fe64b37a05f
shreshth <shreshthsrivastava2@gmail.com>
2020-10-22 13:55:03 -0400

    switched to scss

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import './app.global.css';
+import './app.global.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   render(<App />, document.getElementById('root'));

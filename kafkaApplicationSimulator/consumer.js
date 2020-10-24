a79eab3a83f6ba1defa537779561f6a1df77ecd0
Kaiwei Hsu <hkw1988@hotmail.com>
2020-10-24 12:11:16 -0700

    progress setup kafkaNode

--- kafkaApplicationSimulator/consumer.js
+++ kafkaApplicationSimulator/consumer.js
@@ -1,34 +1,70 @@
-const { Kafka } = require('kafkajs');
-// const msg = process.argv[2];
+// const { Kafka } = require('kafkajs');
+// // const msg = process.argv[2];
 
-run();
-async function run() {
+// run();
+// async function run() {
+//   try {
+//     const kafka = new Kafka({
+//       clientId: 'myapp',
+//       brokers: ['Weis-NB.local:9092'],
+//     });
+//     const consumer = kafka.consumer({
+//       groupId: 'test',
+//     });
+//     console.log('connecting...');
+//     await consumer.connect();
+//     console.log('connected!');
+
+//     await consumer.subscribe({
+//       topic: 'Users',
+//       fromBeginning: true,
+//     });
+
+//     await consumer.run({
+//       eachMessage: async (result) => {
+//         console.log(
+//           `MSG: ${result.message.value} PARTITION: ${result.partition}`
+//         );
+//       },
+//     });
+//   } catch (err) {
+//     console.log(`ERROR: ${err}`);
+//   } finally {
+//   }
+// }
+
+const kafka = require('kafka-node');
+
+const kafkaController = {};
+
+kafkaController.consumer = async (req, res, next) => {
   try {
-    const kafka = new Kafka({
-      clientId: 'myapp',
-      brokers: ['Weis-NB.local:9092'],
-    });
-    const consumer = kafka.consumer({
-      groupId: 'test',
-    });
-    console.log('connecting...');
-    await consumer.connect();
-    console.log('connected!');
+    const Consumer = kafka.Consumer;
+    const client = new kafka.KafkaClient({ kafkaHost: 'Weis-NB.local:9092' });
 
-    await consumer.subscribe({
-      topic: 'Users',
-      fromBeginning: true,
+    const partition = msg[0].toLowerCase() < 'n' ? 0 : 1;
+    const consumer = new Consumer(
+      client,
+      [
+        {
+          topic: 'Users',
+          offset: 0,
+          partition: partition,
+        },
+      ],
+      {
+        autoCommit: false,
+      }
+    );
+    // async?
+    consumer.on('message', function (message) {
+      console.log(`this is the consumer message ${message}`);
     });
 
-    await consumer.run({
-      eachMessage: async (result) => {
-        console.log(
-          `MSG: ${result.message.value} PARTITION: ${result.partition}`
-        );
-      },
+    consumer.on('error', function (err) {
+      console.log(`Consumer Error: ${err}`);
     });
   } catch (err) {
-    console.log(`ERROR: ${err}`);
-  } finally {
+    console.log(err);
   }
-}
+};
--- kafkaApplicationSimulator/controllers/kafkaController.js
+++ kafkaApplicationSimulator/controllers/kafkaController.js
@@ -1,38 +1,76 @@
-const { Kafka } = require('kafkajs');
+// const { Kafka } = require('kafkajs');
+
+// const kafkaController = {};
+
+// kafkaController.produceMessage = async (req, res, next) => {
+//   console.log('hi');
+//   try {
+//     const kafka = new Kafka({
+//       clientId: 'myapp',
+//       brokers: ['Weis-NB.local:9092'],
+//     });
+//     const producer = kafka.producer();
+//     console.log('connecting...');
+//     await producer.connect();
+//     console.log('connected!');
+
+//     const { msg } = req.body;
+//     console.log(msg);
+
+//     const partition = msg[0].toLowerCase() < 'n' ? 0 : 1;
+//     const result = await producer.send({
+//       topic: 'Users',
+//       messages: [
+//         {
+//           value: msg,
+//           partition: partition,
+//         },
+//       ],
+//     });
+//     console.log(`send successfully! ${JSON.stringify(result)}`);
+//     await producer.disconnect();
+//   } catch (err) {
+//     console.error(`ERROR: ${err}`);
+//   } finally {
+//     next();
+//   }
+// };
+
+// module.exports = kafkaController;
+
+const kafka = require('kafka-node');
 
 const kafkaController = {};
 
 kafkaController.produceMessage = async (req, res, next) => {
-  console.log('hi');
   try {
-    const kafka = new Kafka({
-      clientId: 'myapp',
-      brokers: ['Weis-NB.local:9092'],
-    });
-    const producer = kafka.producer();
-    console.log('connecting...');
-    await producer.connect();
-    console.log('connected!');
+    const Producer = kafka.Producer;
+    const client = new kafka.KafkaClient({ kafkaHost: 'Weis-NB.local:9092' });
+    const producer = new Producer(client);
 
-    const { msg } = req.body;
-    console.log(msg);
+    //     const { msg } = req.body;
+    //     console.log(msg);
 
     const partition = msg[0].toLowerCase() < 'n' ? 0 : 1;
-    const result = await producer.send({
-      topic: 'Users',
-      messages: [
-        {
-          value: msg,
-          partition: partition,
-        },
-      ],
+    payloads = [
+      {
+        topic: 'Users',
+        messages: 'AAAHi from kafka-Node',
+        partition: partition,
+      },
+    ];
+    // async?
+    producer.on('ready', function () {
+      producer.send(payloads, function (err, data) {
+        console.log(data);
+      });
     });
-    console.log(`send successfully! ${JSON.stringify(result)}`);
-    await producer.disconnect();
+    console.log(`message sent!`);
+    producer.on('error', function (err) {
+      console.log(`Producer Error: ${err}`);
+    });
   } catch (err) {
-    console.error(`ERROR: ${err}`);
-  } finally {
-    next();
+    console.log(err);
   }
 };
 

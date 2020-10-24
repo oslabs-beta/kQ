7fcb359a7370c0f6ac85759b3d07b9db51957a6e
shreshth <shreshthsrivastava2@gmail.com>
2020-10-24 11:51:21 -0400

    set up tracking methods in kafkaq-monitor

--- kafkaq-monitor/index.js
+++ kafkaq-monitor/index.js
@@ -1,3 +1,18 @@
-const printHello = () => console.log('hello');
+// const printHello = () => console.log('hello');
 
-module.exports = { printHello };
+let num = 0;
+
+const trackProducer = (producer) => {
+  const { REQUEST } = producer.events;
+  const sendListener = producer.on(REQUEST, (e) => {
+    console.log(`REQUEST size: ${e.payload.size}`);
+    console.log(`REQUEST duration: ${e.payload.duration}`);
+    console.log(`REQUEST sentAt: ${e.payload.sentAt}\n`);
+    num += e.payload.size;
+    console.log(`num is: ${num}`)
+  })
+  
+  // printHello();
+}
+
+module.exports = { trackProducer };

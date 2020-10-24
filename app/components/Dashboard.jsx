f9469714b61a9af70e1e90229f2f809fb3e870ef
shreshth <shreshthsrivastava2@gmail.com>
2020-10-24 14:40:41 -0400

    brought in jonathans changes. he set up basic structure for dashboard on frontend

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
 import SystemData from './SystemData';
+/* eslint-disable */
+
+
+const api = {
+  key: '9cf4213d3cc82809c8344f68d1ea107f',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
 
 class Dashboard extends Component {
   // state should probably go in this component
-  render() {
-    return (
-      <div className="app">
-        <h2>THIS IS THE DASHBOARD:</h2>
-        <SystemData />
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+    };
+    this.clickMe = this.clickMe.bind(this);
   }
+
+  // componentDidMount() {
+  //   this.fetchData();
+  // }
+
+  clickMe(e) {
+    alert('button clicked');
+  }
+
+  // fetchData() {
+  //   if (evt.key === 'Enter') {
+  //     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         setWeather(result);
+  //         setQuery('');
+  //       })
+  //       .catch((error) => console.log('fetch request failed', error));
+  //   }
+  // }
+
+  render() {
+
+             return (
+               <div className="dashboard">
+                 <div className="dashTitle">
+                   <h2>THIS IS THE DASHBOARD:</h2>
+                    <form className="dashForm" onSubmit={this.clickMe}>
+                      <input placeholder="search"></input>
+                      <button type="submit" >click me</button>
+                    </form>
+
+                   {/* <input
+               type="text"
+               className="search-bar"
+               placeholder="Search..."
+               onChange={((((e)))) => setQuery(e.target.value)}
+               value={query}
+               onClick={search}
+          /> */}
+          {/* should dynamically render number of SystemData boxes */}
+                 </div>
+                 <div className="system-data-container">
+                   <SystemData className="item" />
+                   <SystemData className="item" />
+                   <SystemData className="item" />
+                 </div>
+               </div>
+             );
+           }
 }
 
 export default Dashboard;
---
+++ app/components/metrics/Graph.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+/* eslint-disable */
+
+class Graph extends Component {
+  render() {
+    return (
+      <div className="graph">
+        <h3>This is a Graph Metric</h3>
+      </div>
+    );
+  }
+}
+
+export default Graph;

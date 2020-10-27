a86087bc474ce2420b2dcc630fdd9f864ca393a9
Jonathan Barenboim <Jonathan.Barenboim@gmail.com>
2020-10-27 15:54:24 -0700

    attempted to finish metric.jsx NOT WORKING

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Dashboard from './Dashboard';
 import Homepage from './Homepage';
+import Icon from './assets/kafkaq_logoTest.svg';
+// import logo from './assets/kafkaq_logoTest.svg';
 
 /* eslint-disable */
 // const api = {
@@ -13,11 +15,13 @@ class App extends Component {
     return (
       <div className="app">
         <div className="title">
+          {/* <img height={25} src={logo} alt="logo" /> */}
           <h2>kafkaQ</h2>
           <h1>Dashboard</h1>
         </div>
         <div className="dashboard-container">
           <Dashboard />
+          <Icon width={50} height={50} />
         </div>
       </div>
     );
--- app/components/templates/Metric.jsx
+++ app/components/templates/Metric.jsx
@@ -10,23 +10,26 @@ const avg =
 
 /-Method to create chartData (params: title, avg) object
 
--Method to create chartOptions object
+/-Method to create chartOptions object
 
 /-Method to create a title
 
--Method to create a label (these are things like avg, smallest, largest)
+/-Method to create a label (these are things like avg, smallest, largest)
 
--Method to create a graph
+/-Method to create a graph
 
 add comments to methods
 
 */
-
+// Metric class does...
 class Metric extends Component {
   constructor() {
     this.backgroundColor = 'rgb(0, 195, 255)';
     this.borderWidth = 2;
     this.borderColor = 'rgb(36, 36, 36)';
+    this.fontSize = 30;
+    this.fontColor = 'rgb(36, 36, 36)';
+    this.fontFamily = 'Lato';
   }
 
   // Calculate avg given sum and number of data points
@@ -38,7 +41,7 @@ class Metric extends Component {
         : (sum / numOfDataPoints).toFixed(2);
     )
   }
-
+  // Create chart data
   generateChartData(title, avg) {
     return {
       labels: [title],
@@ -52,9 +55,67 @@ class Metric extends Component {
       ],
     };
   }
-
+  // Create chart title
   generateTitle(title) {
     return <h2 className="metric-title">{title}</h2>
+  }
+
+  // Create a graph
+  generateGraph(chartData, chartOptions){
+    return (<div className="chart">
+            <Bar data={chartData} options={chartOptions} />
+          </div>
+    )
+  }
+
+  // Create a label for items: avg, smallest, largest
+  generateLabes(avg, smallest, largest){
+    return (
+        <p className="metrics">Average: {avg}</p>
+        <p className="metrics">
+          Smallest: {smallest === Number.POSITIVE_INFINITY ? 'None' : smallest}
+        </p>
+        <p className="metrics">
+          Largest: {largest === Number.NEGATIVE_INFINITY ? 'None' : largest}
+        </p>
+    )
+  }
+
+  // Create chartOptions object
+  generateChartOptions(){
+    // const fontSize = 30;
+    // const fontColor = 'rgb(36, 36, 36)';
+    // const fontFamily = 'Lato';
+    return {
+      title: {
+        display: false,
+      },
+      legend: {
+        display: false,
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              this.fontSize,
+              this.fontColor,
+              thisfontFamily,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              this.fontSize,
+              this.fontColor,
+              this.fontFamily,
+            },
+          },
+        ],
+      },
+    };
   }
 
   render() {

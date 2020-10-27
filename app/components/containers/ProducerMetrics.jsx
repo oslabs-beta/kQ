01f0e2ec4bfd74243004a7844ff21dc8b8a8cb90
shreshth <shreshthsrivastava2@gmail.com>
2020-10-27 12:24:44 -0400

    make new folder and files

---
+++ app/components/containers/ProducerMetrics.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ProducerMetrics extends Component {
+  render() {
+    <h1>Producer Metrics</h1>
+    <p>put stuff here</p>
+  }
+}
+
+export default ProducerMetrics;

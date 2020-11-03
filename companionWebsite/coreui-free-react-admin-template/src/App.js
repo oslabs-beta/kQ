d5e35cfd9167cfbf62121f632f2543f6de3586d5
shreshth <shreshthsrivastava2@gmail.com>
2020-11-03 09:58:59 -0500

    coreui template working

---
+++ companionWebsite/coreui-free-react-admin-template/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './scss/style.scss';
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+);
+
+// Containers
+const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'));
+const Register = React.lazy(() => import('./views/pages/register/Register'));
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+
+class App extends Component {
+  render() {
+    return (
+      // <HashRouter>
+      //   <React.Suspense fallback={loading}>
+      //     <Switch>
+      //       <Route
+      //         exact
+      //         path="/login"
+      //         name="Login Page"
+      //         render={(props) => <Login {...props} />}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/register"
+      //         name="Register Page"
+      //         render={(props) => <Register {...props} />}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/404"
+      //         name="Page 404"
+      //         render={(props) => <Page404 {...props} />}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/500"
+      //         name="Page 500"
+      //         render={(props) => <Page500 {...props} />}
+      //       />
+      //       <Route
+      //         path="/"
+      //         name="Home"
+      //         render={(props) => <TheLayout {...props} />}
+      //       />
+      //     </Switch>
+      //   </React.Suspense>
+      // </HashRouter>
+      <HashRouter>
+        <React.Suspense fallback={loading}>
+          <Switch>
+            <Route
+              path="/"
+              name="Home"
+              render={(props) => <TheLayout {...props} />}
+            />
+          </Switch>
+        </React.Suspense>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;

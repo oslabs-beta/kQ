855a572dc86fe33d831c3b731d26e7cf230bf150
ymturan <ymturan1@gmail.com>
2020-11-05 01:53:52 -0500

    added comments to the backend

--- kafkaApplicationSimulator/controllers/kafkaController.js
+++ kafkaApplicationSimulator/controllers/kafkaController.js
@@ -4,18 +4,29 @@ require('dotenv').config();
 
 const kafkaController = {};
 
+// creates producers and establishes a connection to the Kafka broker
 kafkaController.produceMessage = async (req, res, next) => {
   console.log('hi');
   try {
+
+    // establishing a TCP conncetion to be able to communicate with the broker
     const kafka = new Kafka({
-      clientId: 'myapp',
-      brokers: [`${process.env.LOCAL_COMPUTER}:9092`],
+      clientId: 'myapp', // creates a client name
+      brokers: [`${process.env.LOCAL_COMPUTER}:9092`], // initiate your broker, in this case your local machine + port
     });
+
+    // pull the producer object in order to create a producer 
     const producer = kafka.producer();
     console.log('connecting...');
+
+    // connect your producer to the broker 
     await producer.connect();
     console.log('connected!');
+
+    // invokes the imported npm module to track producer data 
     trackProducer(producer);
+
+    // deconstruct the object that holds the message produced by the Golang server
     const { msg } = req.body;
     // console.log(msg);
 
@@ -30,13 +41,16 @@ kafkaController.produceMessage = async (req, res, next) => {
 
     // console.log(` BEFORE -------> ${trackProducer(producer)}`);
 
+    // assign a value (0 or 1) to the variable based on the condition, given the message produced by Golang server
     const partition = msg[0].toLowerCase() < 'n' ? 0 : 1;
+
+    // distributes the messages to the topics and its partition
     const result = await producer.send({
-      topic: 'Users',
+      topic: 'Users',   // send to User topic declared previously 
       messages: [
         {
-          value: msg,
-          partition: partition,
+          value: msg, // the actual message produced
+          partition: partition, // specifies to which partition the message needs to be send to
         },
       ],
     });
@@ -44,6 +58,7 @@ kafkaController.produceMessage = async (req, res, next) => {
     // console.log(` AFTER -------> ${trackProducer(producer)}`);
 
     console.log(`send successfully! ${JSON.stringify(result)}`);
+
     await producer.disconnect();
   } catch (err) {
     console.error(`ERROR: ${err}`);
--- kafkaq-monitor/index.js
+++ kafkaq-monitor/index.js
@@ -3,21 +3,30 @@ const axios = require('axios');
 
 // let num = 0;
 
+// function invoked in kafka topic.js 
 const trackProducer = (producer) => {
   const { REQUEST } = producer.events;
   const sendListener = producer.on(REQUEST, (e) => {
     const url = 'http://localhost:5000/producer';
+
+    // producer data that are being tracked on the kafka producer
     const { size, pendingDuration, sentAt } = e.payload;
     const data = { size, pendingDuration, sentAt };
+
+    // create a HTTP connection to server.js (kafkaQ server) to endpoint :5000/producer and send over the data object
     axios.post(url, { data });
   });
 };
 
-// create for consumer
+// function invoked in kafka consumer.js 
 const trackConsumer = (consumer) => {
   const { REQUEST } = consumer.events;
+
+  // kafkajs method to receive events from consumers  
   const sendListener = consumer.on(REQUEST, (e) => {
     const url = 'http://localhost:5000/consumer';
+
+    // consumer data that are being tracked on the kafka consumers
     const { size, pendingDuration, sentAt } = e.payload;
     const data = { size, pendingDuration, sentAt };
     console.log(
@@ -25,6 +34,8 @@ const trackConsumer = (consumer) => {
     );
     console.log('consumerPendingDuration:', pendingDuration);
     // console.log(`data: ${Object.keys(data)}`);
+
+    // create a HTTP connection to server.js (kafkaQ server) to endpoint :5000/consumer and send over the data object
     axios.post(url, { data });
 
     console.log(`Consumer REQUEST size: ${e.payload.size}`);

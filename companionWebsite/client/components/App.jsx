43934098cb6fc92abdabf071ff5ee52023f54724
shreshth <shreshthsrivastava2@gmail.com>
2020-11-05 16:47:55 -0500

    finished home page

--- companionWebsite/client/components/App.jsx
+++ companionWebsite/client/components/App.jsx
@@ -8,9 +8,12 @@ import Docs from './Docs.jsx';
 import Home from './Home.jsx';
 import QuickStart from './QuickStart.jsx';
 
+import kafkaQlogoWithoutLetters from '../icons/kafkaQ-logo-no-letters.png';
 import gmailLogo from '../icons/gmail-logo.png';
 import twitterLogo from '../icons/twitter-logo.png';
 import githubLogo from '../icons/github-logo.png';
+import mediumLogo from '../icons/medium-logo.png';
+import npmLogo from '../icons/npm-logo.png';
 
 // $bright-white: #f9f9fc;
 // $dim-white: #d1d1d2;
@@ -86,18 +89,20 @@ class App extends Component {
           </Switch>
 
           {/* Footer */}
-          {/* PROBABLY REPLACE ALL THESE WITH LOGOS */}
           <footer className="box-background">
             {/* kafkaQ info */}
-            <div className="box-background">
-              {/* logo */}
-              <h3 className="box-background">kafkaQ</h3>
+            <div className="box-background footer-title">
+              <img
+                className="box-background"
+                src={kafkaQlogoWithoutLetters}
+                width="50"
+                height="42"
+              />
+              <h3 className="box-background title">kafkaQ</h3>
             </div>
 
-            {/* Contact us */}
-            <div className="box-background">
-              {/* <strong className="box-background">Email: </strong>
-              kafkaQ21@gmail.com */}
+            <div className="logos">
+              {/* Gmail */}
               <a
                 className="box-background"
                 href="https://twitter.com/kafkaq21gmailc1"
@@ -105,63 +110,65 @@ class App extends Component {
                 <img
                   className="box-background"
                   src={gmailLogo}
-                  width="60"
-                  height="50"
+                  width="40"
+                  height="34"
                 />
               </a>
+
+              {/* Twitter */}
               <a
                 className="box-background"
+                id="twitter-logo"
                 href="https://twitter.com/kafkaq21gmailc1"
               >
                 <img
                   className="box-background"
                   src={twitterLogo}
-                  width="60"
+                  width="40"
+                  height="34"
+                />
+              </a>
+
+              {/* GitHub repo */}
+              <a
+                className="box-background"
+                href="https://github.com/oslabs-beta/kafkaQ"
+              >
+                <img
+                  className="box-background"
+                  src={githubLogo}
+                  width="70"
+                  height="49"
+                />
+              </a>
+
+              {/* Medium article */}
+              <a
+                className="box-background"
+                id="medium-logo"
+                href="https://www.medium.com"
+              >
+                <img
+                  className="box-background"
+                  src={mediumLogo}
+                  width="67"
                   height="50"
                 />
               </a>
-              <strong className="box-background"> Twitter: </strong>#kafkaQ
-            </div>
 
-            {/* GitHub repo */}
-            <a
-              className="box-background"
-              href="https://github.com/oslabs-beta/kafkaQ"
-            >
-              <img
+              {/* NPM Package */}
+              <a
                 className="box-background"
-                src={githubLogo}
-                width="75"
-                height="50"
-              />
-            </a>
-
-            <div className="box-background">
-              <strong className="box-background">GitHub Repo: </strong>{' '}
-              https://github.com/oslabs-beta/kafkaQ
+                href="https://www.npmjs.com/package/kafkaq-monitor"
+              >
+                <img
+                  className="box-background"
+                  src={npmLogo}
+                  width="73"
+                  height="34"
+                />
+              </a>
             </div>
-
-            {/* Medium article */}
-            <div className="box-background">
-              <strong className="box-background">Medium article: </strong>{' '}
-              https://www.medium.com
-            </div>
-
-            {/* NPM Package */}
-            <div className="box-background">
-              <strong className="box-background">NPM Package: </strong>{' '}
-              https://www.npmjs.com/package/kafkaq-monitor
-            </div>
-            {/* <p>We are kafkaQ</p>
-            <div>
-              Contact us:
-              <a href="https://www.gmail.com">kafkaQ21@gmail.com </a>
-              <br />
-              Twitter:
-              <a href="https://www.twitter.com">#kafkaQ</a>
-            </div>
-            Join us:
-            <a href="https://codesmith.io/">Career</a> */}
           </footer>
         </div>
       </Router>

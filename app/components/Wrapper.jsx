f41e851aae47113316149c9e1aac461cd2b156c0
Jonathan Barenboim <Jonathan.Barenboim@gmail.com>
2020-11-05 18:17:48 -0800

    began ui design on electron ui

--- app/components/Wrapper.jsx
+++ app/components/Wrapper.jsx
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import Dashboard from './Dashboard';
 import Homepage from './Homepage';
-
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 /* eslint-disable */
 
-// Renders the titles and the DASHBOARD component
+//Renders the titles and the DASHBOARD component
 class Wrapper extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="title">
-          <h2>kafkaQ</h2>
-          <h1>Dashboard</h1>
-        </div>
-        <div className="dashboard-container">
-          <Dashboard />
-        </div>
+      <div>
+        <Container fluid>
+          <Row>
+            <Col>
+              <h2>kafkaQ</h2>
+            </Col>
+            <Col>
+              <h1>Dashboard</h1>
+            </Col>
+            <Col>
+              <h2>User</h2>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Col>
+            <Dashboard />
+          </Col>
+        </Container>
       </div>
+      // <div className="app">
+      //   <div className="title">
+      //     <h2>kafkaQ</h2>
+      //     <h1>Dashboard</h1>
+      //   </div>
+      //   <div className="dashboard-container">
+      //     <Dashboard />
+      //   </div>
+      // </div>
     );
   }
 }
--- app/components/containers/ProducerMetrics.jsx
+++ app/components/containers/ProducerMetrics.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Metric from '../Metric.jsx';
+import { Container, Col, Row } from 'react-bootstrap';
 // import DataSize from '../metrics/producer/DataSize.jsx';
 // import ProcessingTime from '../metrics/producer/ProcessingTime.jsx';
 
@@ -11,15 +12,35 @@ class ProducerMetrics extends Component {
     const { dataSize, processingTime, pendingDuration } = this.props;
 
     return (
-      <div className="dashboard">
-        <h1 className="metric-heading">Producer Metrics</h1>
-        <div className="metrics-container">
-          <Metric title={'Data Size'} data={dataSize} />
-          <Metric title={'Processing Time'} data={processingTime} />
-          <Metric title={'Pending Duration'} data={pendingDuration} />
-        </div>
-        {/* <DataSize />
+      <div>
+        {/* <div className="dashboard"> */}
+        <Container>
+          <Container>
+            <Row>
+              <Col>
+                {/* <h1 className="metric-heading">Producer Metrics</h1> */}
+                <h1>Producer Metrics</h1>
+              </Col>
+            </Row>
+          </Container>
+          {/* <div className="metrics-container"> */}
+          <Container>
+            <Row>
+              <Col>
+                <Metric title={'Data Size'} data={dataSize} />
+              </Col>
+              <Col>
+                <Metric title={'Processing Time'} data={processingTime} />
+              </Col>
+              <Col>
+                <Metric title={'Pending Duration'} data={pendingDuration} />
+              </Col>
+            </Row>
+          </Container>
+          {/* </div> */}
+          {/* <DataSize />
         <ProcessingTime /> */}
+        </Container>
       </div>
     );
   }

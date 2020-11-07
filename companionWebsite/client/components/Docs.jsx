bc6f98fc2b1dabf53f844e04b4f92bdea307a7d2
ymturan <ymturan1@gmail.com>
2020-11-07 17:57:19 -0500

    webDocs iin progress

--- companionWebsite/client/components/Docs.jsx
+++ companionWebsite/client/components/Docs.jsx
@@ -19,15 +19,33 @@ class Docs extends Component {
         </p>
         <p className="box-background">
           <code className="box-background code">
-            const {'{'} trackProducer {'}'} =
-            require('../../kafkaq-monitor/index.js')
+            <span class="token keywords">const</span>
+            <span class="token symbols"> {'{'} </span>
+            <span class="token names"> trackProducer </span>
+            <span class="token symbols"> {'}'} </span>
+            <span class="token equation"> = </span>
+            <span class="token keywordReq"> require </span>
+            <span class="token symbols">(</span>
+            <span class="token symbols">'</span>
+            <span class="token string">../../kafkaq-monitor/index.js</span>
+            <span class="token symbols">'</span>
+            <span class="token symbols">)</span>
+            <span class="token symbols">;</span>
           </code>
         </p>
         <p className="box-background">
           Then, invoke trackProducer with your producer as the argument after
           your producer is connected.
+          <p />
+          <p className="box-background"></p>
+          <code className="box-background code">
+            <span class="token func"> trackProducer</span>
+            <span class="token symbols">(</span>
+            <span class="token names">producer</span>
+            <span class="token symbols">)</span>
+            <span class="token symbols">;</span>
+          </code>
         </p>
-        <code className="box-background">trackProducer(producer)</code>
         <h3 className="box-background title">Consumer</h3>
         <p className="box-background">
           To monitor performance data from your Kafka consumer, import
@@ -35,21 +53,38 @@ class Docs extends Component {
         </p>
         <p className="box-background">
           <code className="box-background code">
-            const {'{'} trackConsumer {'}'} =
-            require('../../kafkaq-monitor/index.js')
+            <span class="token keywords">const</span>
+            <span class="token symbols"> {'{'} </span>
+            <span class="token names"> trackConsumer </span>
+            <span class="token symbols"> {'}'} </span>
+            <span class="token equation"> = </span>
+            <span class="token keywordReq"> require </span>
+            <span class="token symbols">(</span>
+            <span class="token symbols">'</span>
+            <span class="token string">../../kafkaq-monitor/index.js</span>
+            <span class="token symbols">'</span>
+            <span class="token symbols">)</span>
+            <span class="token symbols">;</span>
           </code>
         </p>
         <p className="box-background">
           Then, invoke trackConsumer with your consumer as the argument after
           your consumer is connected.
         </p>
-        <code className="box-background">trackConsumer(consumer)</code>
-
+        <p className="box-background">
+          <code className="box-background code">
+            <span class="token func"> trackConsumer</span>
+            <span class="token symbols">(</span>
+            <span class="token names">consumer</span>
+            <span class="token symbols">)</span>
+            <span class="token symbols">;</span>
+          </code>
+        </p>
         <h2 className="box-background">Testing</h2>
         <p className="box-background">
           run docker, zookeeper, start kafka cluster, and run test app
         </p>
-      </div>
+      </div >
     );
   }
 }

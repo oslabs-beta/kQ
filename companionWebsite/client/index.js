a8966127c6a93d0ec78adc196d99183b1dbb4e20
shreshth <shreshthsrivastava2@gmail.com>
2020-11-07 19:36:16 -0500

    renamed app.jsx

--- companionWebsite/client/index.js
+++ companionWebsite/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
+import App from './components/App.tsx';
 import 'semantic-ui-css/semantic.min.css';
 import styles from './styles/styles.scss';
 

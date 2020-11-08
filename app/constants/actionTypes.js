0b3ad23fe5b855e7c305f6ce4b732c1f07dd64be
shreshth <shreshthsrivastava2@gmail.com>
2020-11-08 13:45:01 -0500

    finished refactoring frontend to typescript

--- app/constants/actionTypes.js
+++
@@ -1,4 +0,0 @@
-// Defining action types as constants that describe how they intend to mutate data
-// for producer and consumer metrics
-export const ADD_PRODUCER_DATA = 'ADD_PRODUCER_DATA';
-export const ADD_CONSUMER_DATA = 'ADD_CONSUMER_DATA';
